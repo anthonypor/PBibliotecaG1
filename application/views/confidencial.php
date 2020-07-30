e9c153d27803b6f06e6b935abb8ccbba47fa82c6
Franklin-JAE <Arias_LJML@HOTMAIL.COM>
2020-07-29 21:31:33 -0500

    Actualizacion login

---
+++ application/views/confidencial.php
@@ -0,0 +1,24 @@
+<p>
+    Usuario: <?php echo $usuario->usuario;?>
+</p>
+<p>
+    Password: <?php echo $usuario->password;?>
+</p>
+<p>
+    ID: <?php echo $usuario->usua_id;?>
+</p>
+<p>
+    Nombres: <?php echo $usuario->usua_nombres;?>
+</p>
+<p>
+    Apellidos: <?php echo $usuario->usua_apellidos;?>
+</p>
+<p>
+    Direccion: <?php echo $usuario->usua_direccion;?>
+</p>
+<p>
+    Email: <?php echo $usuario->usua_email;?>
+</p>
+<p>
+    Telefono: <?php echo $usuario->usua_telefono;?>
+</p>
