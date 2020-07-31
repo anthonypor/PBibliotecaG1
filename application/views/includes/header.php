968d53ac82265436315ad1f89bab1da2d0fe8672
Fiorella <Yukiakirafio@gmail.com>
2020-07-31 10:37:18 -0500

    Ultimate

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -9,32 +9,34 @@
 <body>
 <div class="container">
 <div>
-<h1 align="center" class="text-white bg-dark"> Biblioteca </h1>
-</div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  
-  
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-      <a class="nav-link" href="welcome">Inicio <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-      <a class="nav-link" href="login">Login<span class="sr-only">(current)</span></a>
-      </li>
-      <a class="nav-link" href="usuario">Usuarios <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-      <a class="nav-link" href="ejemplar">Ejemplares <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active">
-      <a class="nav-link" href="categoria">Categoria <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+<h1 align="center" class="text-white bg-dark"><font size="9"  face="Comic Sans MS"><b><i class="fas fa-book-reader"></i> BIBLIOTECA <i class="fas fa-book-reader"></i></b></font></h1>
+</div>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<div class="container">  
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="container">
+  <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link" href="welcome"><font size="4"  face="Comic Sans MS"><b><i class="fa fa-play" aria-hidden="true"></i> Inicio </b></font></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="login"><font size="4"  face="Comic Sans MS"><b><i class="fas fa-sign-in-alt"></i> Login</b></font></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="usuario"><font size="4"  face="Comic Sans MS"><b><i class="fas fa-users"></i> Usuarios</b></font></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="ejemplar"><font size="4"  face="Comic Sans MS"><b><i class="fas fa-book"></i> Ejemplares</b></font></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="categoria"><font size="4"  face="Comic Sans MS"><b><i class="fas fa-bookmark"></i> Categoría</b></font></a>
+  </li>
+</ul>
   </div>
 </nav>
