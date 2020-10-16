7211540a1ad6dbccd9dbb7f4cc7207d32bd3c3eb
Fio <50608048+kushiro18@users.noreply.github.com>
2020-10-16 03:14:07 -0500

    Update Reportes.php

--- application/controllers/Reportes.php
+++ application/controllers/Reportes.php
@@ -9,17 +9,24 @@ class Reportes extends CI_Controller {
             $this->load->library('Pdf');
             $pdf = new PDF();
             $pdf->AliasNbPages();
-            $pdf->AddPage();
+            $pdf->AddPage('LANDSCAPE');
             $pdf->SetFont('Arial','B',12);
+        
+            
             $rows = $this->db->query("SELECT * FROM ejemplar1")->result();
+        
             foreach($rows as $row){
-                $pdf->Cell(60,10,$row->ejem_titulo,1);
-                $pdf->Cell(61,10,$row->ejem_editorial,1);
-                $pdf->Cell(30,10,$row->ejem_idioma,1);
-                $pdf->Cell(40,10,$row->ejem_anio,1);
-                $pdf->ln(10);
+     
+                $pdf->Cell(60,50,$row->ejem_titulo,1,'','C');
+                $pdf->Cell(61,50,utf8_decode($row->ejem_editorial),1,'','C');
+                $pdf->Cell(30,50,utf8_decode($row->ejem_idioma),1,'','C');
+                $pdf->Cell(40,50,utf8_decode($row->ejem_anio),1,'','C');
+                $pdf->Cell(87,50,$pdf->Image('https://static.wikia.nocookie.net/beekeepers21/images/c/c8/NotFound.png', $pdf->GetX()+25, $pdf->GetY()+5,40),1);
+               
+                $pdf->ln(50);
                 
             }
-            $pdf->Output();
+            
+            $pdf->Output('','Reporte semanal');
         }	
 }
