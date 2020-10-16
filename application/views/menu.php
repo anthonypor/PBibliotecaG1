060e33ad408a44842cf0ad3f49a8514022e13f46
Frankin-JAE <arias_ljml@hotmail.com>
2020-10-15 23:57:11 -0500

    Inicio

--- application/views/menu.php
+++ application/views/menu.php
@@ -1,27 +1,60 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-<nav class="navbar navbar-expand-lg navbar-light bg-navbar-light" style="background-color: #e3f2fd;">
-  <a class="navbar-brand" href="<?php echo base_url(); ?>" >inicio</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo base_url('Welcome/confidencial'); ?>"> Informacion confidencial </a>
+<br><br><br><br>
+<div class="table-responsive">
+    <img align="right" src="https://s03.s3c.es/imag/_v0/770x420/9/0/3/600x400_770x420-biblioteca-cambio-de-rol-dreamstime.jpg" width="550" height="370">
+    <h4>
+    <dl>
+      <dt>LA BIBLIOTECA VIRTUAL:</dt>
+      <dd> donde encotraras las siguientes categorias:</dd>
+      <br>
+      <dd><li>Accion.</li></dd>
+      <dd><li>Audio Libros.</li></dd>
+      <dd><li>Comedia.</li></dd>
+      <dd><li>Folletos.</li></dd>
+      <dd><li>Libros.</li></dd>
+      <dd><li>Novelas.</li></dd>
+      <dd><li>Revistas.</li></dd>
+      </h4>
+      <br><br><br><br><br><br>
+      <img align="left" src="https://i1.wp.com/www.marcelopedra.com.ar/blog/wp-content/uploads/2014/10/ebooks-720.jpg?fit=720%2C576&ssl=1" width="550" height="370">
+      <h4>
+      <dt>Novedades</dt>
+      <dd>
+      <li>
+      Este proyecto busca aprovechar mejor la alta tecnología de los lectores de libros modernos y sus diferentes funciones. De ahí que se ponga especial
+       interes en las tipografías modernas, metadados detallados con enlaces a fuentes de enciclopedias para los lectores más curiosos, y soporte para tablas 
+       de contenido, notas de pie de página, alta resolción y gráficos vectoriales, además de portadas de calidad.
+      Puedes navegar la librería por orden de lanzamiento, alfábetico o por las categorias. </dd>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('Welcome/confidencial2'); ?>">Usuarios</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('Login'); ?>">login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="<?php echo base_url('Login/salir'); ?>" tabindex="-1" aria-disabled="true">logout</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-<div >
-    <img src="https://images8.alphacoders.com/718/718915.jpg"width="1400" height="550">
+      </dl>
+      </h4>
+      <br><br><br><br><br><br>
+      <h4 align="center">
+      <dt>LIBROS EN VENTA</dt>
+      </h4>
+      <table class="table">
+  <thead class="container">
+    <tr>
+      <th scope="col"><img src="https://edit.org/img/blog/xwdn-editar-portadas-de-libros-gratis.jpg.pagespeed.ic.PmNhyGc59r.jpg"width="230" height="330"></th>
+      <th scope="col"><img src="https://edit.org/img/blog/xppe-crear-portadas-de-libros-online.jpg.pagespeed.ic.4gxfEa_h1k.jpg"width="230" height="330"></th>
+      <th scope="col"><img src="https://s3.amazonaws.com/virginia.webrand.com/virginia/344/LEqKdMKtOj8/c9e74d28a1a2e698f62446b8e5345254.jpg"width="230" height="330"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><img src="https://zumografica.com/wp-content/uploads/2017/09/libro-imprenta-lima.jpg"width="230" height="330"></td>
+      <td><img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/sci-fi-book-cover-template-a1ec26573b7a71617c38ffc6e356eef9_screen.jpg?ts=1561547637"width="230" height="330"></td>
+      <td><img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/contemporary-fiction-night-time-book-cover-design-template-1be47835c3058eb42211574e0c4ed8bf_screen.jpg?ts=1594616847"width="230" height="330"></td>
+    </tr>
+    <tr>
+      <td><img src="https://lh3.googleusercontent.com/proxy/yV2GfMS12ofSMSNJDtMBX1HerXHt7IVehv3ZktQBlHYGx8Qy5rtZXvkOjVdVpADoKt9dvRbLPKGEg5O57lKm3gLV9yfALEA_g_czfc5voHgcxzMyavOEwbEonBd25Djc7rxndSIafyAyBi6ORvWynuG5dD8TtQFO7OTppEPzCfrhXNGebziz0Uje4Q"width="230" height="330"></td>
+      <td><img src="https://cdn.flipsnack.com/landing/files/h_1"width="230" height="330"></td>
+      <td><img src="https://cdn.domestika.org/c_limit,dpr_auto,f_auto,q_auto,w_820/v1547929698/content-items/002/728/739/Captura_de_pantalla_2019-01-19_a_las_21.22.39-original.png?1547929698"width="230" height="330"></td>
+    </tr>
+    <tr>
+      <td><img src="https://cdn.cp.adobe.io/content/2/rendition/b2047b6c-5fcc-4cc7-9b44-facd797c43f7/artwork/b3b6a52d-bcbe-40d7-9a64-216dcd1e4c28/version/0/format/jpg/dimension/width/size/300"width="230" height="330"></td>
+      <td><img src="https://s3.amazonaws.com/virginia.webrand.com/virginia/344/KCVmVk4cdl7/202852714dec217e579db202a977be70.jpg"width="230" height="330"></td>
+      <td><img src="https://i.pinimg.com/originals/a3/1c/5c/a31c5ca707c2793cd338f3200707db0e.jpg"width="230" height="330"></td>
+    </tr>
+  </tbody>
+</table>
 </div>
